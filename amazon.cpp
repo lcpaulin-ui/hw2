fe25aafc329a83cc505989b6d48632505d904066
Lucia Paulino <lcpaulin@usc.edu>
2025-10-03 00:54:27 -0700

    ssss

--- amazon.cpp
+++ amazon.cpp
@@ -202,7 +202,7 @@ void displayProducts(vector<Product*>& hits)
     }
     std::sort(hits.begin(), hits.end(), ProdNameSorter());
     for(vector<Product*>::iterator it = hits.begin(); it != hits.end(); ++it) {
-        cout << "Item" << setw(3) << resultNo << endl;
+        cout << "Item " << resultNo << endl;
         cout << (*it)->displayString() << endl;
         cout << endl;
         resultNo++;
